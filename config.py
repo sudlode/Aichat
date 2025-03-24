e29268e66bbb9afe2262df632cacd5a935177e8e
sudlode <sudlodenis@gmail.com>
2025-03-24 23:14:19 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,12 @@
 # config.py
+import os
+from dotenv import load_dotenv
+
+# Завантажуємо змінні середовища з файлу .env
+load_dotenv()
 
 # Токен вашого Telegram бота
-BOT_TOKEN = "7738138408:AAEMrBTn7b-G4I483n_f2b7ceKhl2eSRkdQ"
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 # Ваш OpenAI API ключ
-OPENAI_API_KEY = "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"
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
