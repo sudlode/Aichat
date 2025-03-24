a00ea3d381ae36b9932214667e22f0fd5d0a1dd3
sudlode <sudlodenis@gmail.com>
2025-03-24 23:02:11 +0200

    config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+# config.py
+
+# Токен вашого Telegram бота
+BOT_TOKEN = "7738138408:AAEMrBTn7b-G4I483n_f2b7ceKhl2eSRkdQ"
+
+# Ваш OpenAI API ключ
+OPENAI_API_KEY = "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"
